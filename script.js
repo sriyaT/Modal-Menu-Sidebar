855577d58cb0b0e3bbe134a71551fb4568dc34aa
Sriya <tsriya6@gmail.com>
2020-07-17 20:24:59 +0530

    updated with hide modal on click of sumit button with non empty input field check

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const toggle = document.getElementById('toggle');
 const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
+const submit = document.getElementById('submit-btn');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const password2Input = document.getElementById('password2');
 
 // Toggle Nav
 toggle.addEventListener('click', () => {
@@ -13,6 +18,19 @@ open.addEventListener('click', () => modal.classList.add('show-modal'));
 
 // Hide Modal
 close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+//Hide Modal
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (
+    nameInput.value.length > 0 &&
+    emailInput.value.length > 0 &&
+    passwordInput.value.length > 0 &&
+    password2Input.value.length > 0
+  ) {
+    modal.classList.remove('show-modal');
+  }
+});
 
 // Hide modal on outside click
 window.addEventListener('click', (e) =>
